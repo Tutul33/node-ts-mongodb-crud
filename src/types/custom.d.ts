815ad601e1ce3cd9bb567a190f984d3f92d9a877
Tutul33 <tutulcou@gmail.com>
2025-06-15 14:44:30 +0600

    commit

--- src/types/custom.d.ts
+++
@@ -1,9 +0,0 @@
-import { JwtPayload } from 'jsonwebtoken';
-import { Request } from 'express';
-
-// Extending the Express Request interface
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: string | JwtPayload;  // Adjust type based on your JWT payload structure
-  }
-}
